e7877c27590117fa07dab7fff8e705ff6cc9a4b6
Hakan Karanfil <hakan.karanfil@univera.com.tr>
2022-12-28 01:16:20 +0300

    EventBus projesi oluşturuldu.

---
+++ SellingBuddy/src/BuildingBlocks/EvenBus/EventBus.Base/Events/BaseEventBus.cs
@@ -0,0 +1,32 @@
+﻿using EventBus.Base.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events
+{
+    public abstract class BaseEventBus : IEventBus
+    {
+        public void Publish(IntegrationEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new NotImplementedException();
+            //awsasdasd
+        }
+
+        public void UnSubscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
